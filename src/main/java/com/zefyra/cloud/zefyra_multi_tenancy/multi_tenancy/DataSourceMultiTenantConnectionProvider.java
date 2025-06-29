395c8483c3f2eb2f9f34f74dc0e29115d4704c02
Matteo Pagano <metiupaga8@gmail.com>
2025-06-29 15:50:11 +0200

    refactor DataSourceMultiTenantConnectionProvider to use Lombok for getters and remove unused methods

--- src/main/java/com/zefyra/cloud/zefyra_multi_tenancy/multi_tenancy/DataSourceMultiTenantConnectionProvider.java
+++ src/main/java/com/zefyra/cloud/zefyra_multi_tenancy/multi_tenancy/DataSourceMultiTenantConnectionProvider.java
@@ -1,5 +1,6 @@
 package com.zefyra.cloud.zefyra_multi_tenancy.multi_tenancy;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.engine.jdbc.connections.spi.AbstractDataSourceBasedMultiTenantConnectionProviderImpl;
@@ -7,15 +8,18 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import java.io.Serial;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Map;
 
+@Getter
 @Primary
 @Service
 @Slf4j
 public class DataSourceMultiTenantConnectionProvider extends AbstractDataSourceBasedMultiTenantConnectionProviderImpl<String> {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Setter
@@ -54,11 +58,4 @@ public class DataSourceMultiTenantConnectionProvider extends AbstractDataSourceB
         return dataSources.containsKey(tenantId);
     }
 
-    public void addDataSource(String tenantId, DataSource dataSource) {
-        dataSources.put(tenantId, dataSource);
-    }
-
-    public Map<String, DataSource> getDataSources() {
-        return dataSources;
-    }
 }
