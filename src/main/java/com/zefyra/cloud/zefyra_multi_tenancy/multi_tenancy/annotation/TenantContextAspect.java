1a9bb916f7974bf26f4589126f6de3b98713a8f8
Matteo Pagano <metiupaga8@gmail.com>
2025-07-06 13:42:23 +0200

    feat: enhance TenantContextAspect to retrieve annotation directly from method signature

--- src/main/java/com/zefyra/cloud/zefyra_multi_tenancy/multi_tenancy/annotation/TenantContextAspect.java
+++ src/main/java/com/zefyra/cloud/zefyra_multi_tenancy/multi_tenancy/annotation/TenantContextAspect.java
@@ -4,6 +4,7 @@ import com.zefyra.cloud.zefyra_multi_tenancy.multi_tenancy.util.TenantContext;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,14 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE) // oppure un valore tipo -100
 public class TenantContextAspect {
-    @Around("@annotation(withTenantContext)")
-    public Object switchTenantContext(ProceedingJoinPoint pjp, WithTenantContext withTenantContext) throws Throwable {
+    @Around("@annotation(com.zefyra.cloud.zefyra_multi_tenancy.multi_tenancy.annotation.WithTenantContext)")
+    public Object switchTenantContext(ProceedingJoinPoint pjp) throws Throwable {
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        WithTenantContext withTenantContext = signature.getMethod().getAnnotation(WithTenantContext.class);
+
         Long originalTenantId = TenantContext.getTenantId();
         try {
+            assert withTenantContext != null;
             TenantContext.setTenantId(withTenantContext.value());
             return pjp.proceed();
         } finally {
