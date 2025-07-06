ee1a8c4ba94abd88b22b2e9132874cb28f2038d0
Matteo Pagano <metiupaga8@gmail.com>
2025-07-06 12:40:26 +0200

    feat: add order annotation to TenantContextAspect for priority execution

--- src/main/java/com/zefyra/cloud/zefyra_multi_tenancy/multi_tenancy/annotation/TenantContextAspect.java
+++ src/main/java/com/zefyra/cloud/zefyra_multi_tenancy/multi_tenancy/annotation/TenantContextAspect.java
@@ -4,10 +4,13 @@ import com.zefyra.cloud.zefyra_multi_tenancy.multi_tenancy.util.TenantContext;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE) // oppure un valore tipo -100
 public class TenantContextAspect {
     @Around("@annotation(withTenantContext)")
     public Object switchTenantContext(ProceedingJoinPoint pjp, WithTenantContext withTenantContext) throws Throwable {
